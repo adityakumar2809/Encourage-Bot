c5768756751a4f1149b901c16f7571fd0e0f2b3e
adityakumar2809 <adityakumar2809@gmail.com>
2020-12-31 10:51:29 +0000

    Add feature to add and delete encouragement quotes and store it in database

--- main.py
+++ main.py
@@ -2,14 +2,37 @@ import discord
 import os
 import requests
 import json
+import random
+from replit import db
 
 client = discord.Client()
+
+sad_word = ['sad', 'depressed', 'unhappy', 'miserable', 'angry']
+starter_encouragements = [
+  'Cheer up!',
+  'Hang in there',
+  'You are a great person / bot!'
+]
 
 def get_quote():
   response = requests.get('https://zenquotes.io/api/random')
   json_data = json.loads(response.text)
   quote = json_data[0]['q'] + ' -' + json_data[0]['a']
   return quote
+
+def update_encouragements(msg):
+  if 'encouragements' in db.keys():
+    encouragements = db['encouragements']
+    encouragements.append(msg)
+    db['encouragements'] = encouragements
+  else:
+    db['encouragements'] = [msg]
+
+def delete_encouragements(index):
+  encouragements = db['encouragements']
+  if len(encouragements) > index:
+    del encouragements[index]
+    db['encouragements'] = encouragements
 
 @client.event
 async def on_ready():
@@ -20,8 +43,31 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$inspire'):
+
+  msg = message.content
+
+  if msg.startswith('$inspire'):
     quote = get_quote()
     await message.channel.send(quote)
+
+  options = starter_encouragements
+  if 'encouragements' in db.keys():
+    options = options + db['encouragements']
+
+  if any(word in msg for word in sad_word):
+    await message.channel.send(random.choice(options))
+
+  if msg.startswith('$new'):
+    encouraging_msg = msg.split('$new ', 1)[1]
+    update_encouragements(encouraging_msg)
+    await message.channel.send('New encouraging message added!')
+
+  if msg.startswith('$del'):
+    encouragements = []
+    if 'encouragements' in db.keys():
+      index = int(msg.split('$del', 1)[1])
+      delete_encouragements(index)
+      encouragements = db['encouragements']
+    await message.channel.send(encouragements)
 
 client.run(os.getenv('TOKEN'))
